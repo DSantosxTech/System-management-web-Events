01ab118c1bdecced172407d2e0cf500becc4c09a
Juliano Santos <128374665+DevSntosx71@users.noreply.github.com>
2023-10-13 13:45:09 -0300

    Criado arquivos EventoController.java , EventoRepository.java , index.html e IndexController.java

--- src/main/java/com/eventosweb/controller/EventoController.java
+++ src/main/java/com/eventosweb/controller/EventoController.java
@@ -1,12 +1,35 @@
 package com.eventosweb.controller;
 
+import com.eventosweb.model.Evento;
+import com.eventosweb.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
-    @GetMapping("/eventos/cadastroevento")
-    public String showEventForm() {
+    @Autowired
+    private EventoRepository er;
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
+    public String form() {
         return "evento/formEvento";
+    }
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+    public String form(Evento evento) {
+        er.save(evento);
+        return "redirect:/cadastrarEvento";
+    }
+
+    @RequestMapping("/eventos")
+    public ModelAndView listaEventos() {
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = er.findAll();
+        mv.addObject("eventos", eventos);
+        return mv;
     }
 }
---
+++ src/main/java/com/eventosweb/repository/EventoRepository.java
@@ -0,0 +1,7 @@
+package com.eventosweb.repository;
+
+import com.eventosweb.model.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento,String> {
+}
